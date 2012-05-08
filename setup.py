09f9cc719bcb784dfcf30c3442e7242534d7404a
Paul Lathrop <paul@tertiusfamily.net>
2012-05-08 10:57:52 -0700

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+### -*- coding: utf-8 -*-
+###
+### © 2012 Krux Digital, Inc.
+### Author: Paul Lathrop <paul@krux.com>
+###
+
+from setuptools import setup, find_packages
+
+setup(name='monitorlib',
+      version="0.0.1",
+      description='Library for creating monitoring scripts/plugins',
+      author='Paul Lathrop',
+      author_email='paul@krux.com',
+      url='https://github.com/krux/monitorlib',
+      packages=find_packages(),
+      tests_require=['nose', 'coverage'],
+      )
