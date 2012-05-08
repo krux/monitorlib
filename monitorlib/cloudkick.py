4b50d71b30eb5eb76a3fb12b9ee4c1bbfeee0372
Paul Lathrop <paul@tertiusfamily.net>
2012-05-08 15:17:36 -0700

    Add cloudkick library with tests.

---
+++ monitorlib/cloudkick.py
@@ -0,0 +1,211 @@
+### -*- coding: utf-8 -*-
+###
+### © 2012 Krux Digital, Inc. All rights reserved.
+### Author: Paul Lathrop <paul@krux.com>
+###
+
+"""
+Functions for creating Cloudkick plugins.
+"""
+
+
+from operator import itemgetter
+
+
+### A dictionary of status => priority pairs. The keys define the
+### valid statuses for cloudkick, the values are the relative priority
+### of status lines with that status. See highest_priority()
+STATUS_PRIORITY = {'ok': 0, 'warn': 1, 'err': 2}
+
+### List of valid metric types. See
+### https://support.cloudkick.com/Creating_a_Plugin
+VALID_METRICS = ['int', 'float', 'gauge', 'string']
+
+### Number of fields in a status line.
+STATUS_FIELD_COUNT = 3
+
+### Index of the status type field in a status line (0-indexed).
+STATUS_TYPE_FIELD = 1
+
+### Number of fields in a metric line.
+METRIC_FIELD_COUNT = 4
+
+### Index of the metric type field in a metric line (0-indexed).
+METRIC_TYPE_FIELD = 2
+
+
+def valid_status_type(status):
+    """
+    Return True if status is a valid Cloudkick status.
+    """
+    return status in STATUS_PRIORITY.iterkeys()
+
+
+def valid_metric_type(metric_type):
+    """
+    Return True if line specifies a valid Cloudkick metric type.
+    """
+    return metric_type in VALID_METRICS
+
+
+def priority(status):
+    """
+    Given a valid status, return the priority of that status.
+    """
+    return valid_status_type(status) and STATUS_PRIORITY[status]
+
+
+def compare_priority(pri_a, pri_b):
+    """
+    Given two statuses:
+
+    * Return -1 if the priority of pri_a is less than that of pri_b.
+
+    * Return 0 if the priorities of the two statuses are equal.
+
+    * Return 1 if the priority of pri_a is greater than that of pri_b.
+    """
+    if priority(pri_a) < priority(pri_b):
+        return -1
+    if priority(pri_a) == priority(pri_b):
+        return 0
+    return 1
+
+
+def get_field(field, line, splits):
+    """
+    Given a field index, a whitespace-separated record line, and the
+    number of splits to make, return the given field of the record
+    (0-indexed).
+    """
+    return line.split(None, splits)[field]
+
+
+def is_status_line(line):
+    """
+    Given an output line, return True if it is a Cloudkick-formatted
+    status line, False otherwise.
+    """
+    return all([line.startswith('status '),
+                valid_status_type(get_field(STATUS_TYPE_FIELD,
+                                            line,
+                                            STATUS_FIELD_COUNT))])
+
+
+def is_metric_line(line):
+    """
+    Given an output line, return True if it is a Cloudkick-formatted
+    metric line, False otherwise.
+    """
+    return all([line.startswith('metric '),
+                valid_metric_type(get_field(METRIC_TYPE_FIELD,
+                                            line,
+                                            METRIC_FIELD_COUNT))])
+
+
+def get_status_type(line):
+    """
+    Given a Cloudkick-formatted status line, return the status type
+    specified by that line. Return False if line is not a
+    Cloudkick-formatted status line.
+    """
+    return is_status_line(line) and get_field(STATUS_TYPE_FIELD,
+                                              line,
+                                              STATUS_FIELD_COUNT)
+
+
+def get_metric_type(line):
+    """
+    Given a Cloudkick-formatted metric line, return the metric type
+    specified by that line. Return False if line is not a
+    Cloudkick-formatted metric line.
+    """
+    return is_metric_line(line) and get_field(METRIC_TYPE_FIELD,
+                                              line,
+                                              METRIC_FIELD_COUNT)
+
+
+def status_tuple(line):
+    """
+    Given a Cloudkick-formatted status line, return a tuple ('status',
+    status, message).
+    """
+    return is_status_line(line) and tuple(line.split(None, (STATUS_FIELD_COUNT - 1)))
+
+
+def status_line(tpl):
+    """
+    Given a tuple of the form ('status', status, message), return a
+    Cloudkick-formatted status line.
+    """
+    line = ' '.join(tpl)
+    return is_status_line(line) and line
+
+
+def metric_tuple(line):
+    """
+    Given a Cloudkick-formatted metric line, return a tuple ('metric',
+    name, type, value).
+    """
+    return is_metric_line(line) and tuple(line.split(None, (METRIC_FIELD_COUNT - 1)))
+
+
+def metric_line(tpl):
+    """
+    Given a tuple of the form ('metric', name, type, value), return a
+    Cloudkick-formatted metric line.
+    """
+    line = ' '.join(tpl)
+    return is_metric_line(line) and line
+
+
+def get_status_lines(output_lines):
+    """
+    Given a list of output lines, return a list containing only the
+    status line(s).
+    """
+    return [l for l in output_lines if is_status_line(l)]
+
+
+def get_metric_lines(output_lines):
+    """
+    Given a list of output lines, return a list containing only the
+    metric lines(s).
+    """
+    return [l for l in output_lines if is_metric_line(l)]
+
+
+def sort_by_priority(lines):
+    """
+    Given a list of status lines, return them sorted by descending
+    priority. If any line in lines is not a status line, it will be
+    filtered out of the results.
+    """
+    return list(reversed([status_line(tpl) for tpl in
+                          sorted([status_tuple(line) for line in
+                                  get_status_lines(lines)],
+                                 cmp=compare_priority,
+                                 key=itemgetter(1))]))
+
+
+def highest_priority(lines):
+    """
+    Given a list of lines, return the status line with the highest
+    priority.
+    """
+    return sort_by_priority(get_status_lines(lines))[0]
+
+
+def add_status(line, output_lines):
+    """
+    Adds a status line to a list of output lines. If another status
+    line is already present and the new status has an equal or higher
+    priority, over-write the existing status.
+    """
+    status = highest_priority([line] + output_lines)
+    return [status] + [l for l in output_lines if not is_status_line(l)]
+
+
+if __name__ == '__main__':
+    import nose
+    nose.main()
---
+++ monitorlib/test/test_cloudkick.py
@@ -0,0 +1,200 @@
+### -*- coding: utf-8 -*-
+###
+### © 2012 Krux Digital, Inc. All rights reserved.
+### Author: Paul Lathrop <paul@krux.com>
+###
+
+"""
+Tests for monitorlib.cloudkick
+"""
+
+
+from operator import itemgetter
+
+import nose.tools as test
+
+import monitorlib.cloudkick as ck
+
+
+test.neq_ = test.assert_not_equal
+INVALID = ['invalid', 'wont work', 10, iter('foo')]
+STATUS_CASES = ['status ok this is a valid ok status line',
+                'status warn this is a valid warning status line',
+                'status err this is a valid error status line',
+                'status foo this is a valid status line with an invalid status type',
+                'broken this is not a valid status line'
+                'status tooshort']
+STATUS_EXCEPTIONS = [(1000, AttributeError),
+                     (iter('foo'), AttributeError)]
+METRIC_CASES = ['metric one int 1',
+                'metric two string tea for two and two for tea',
+                'this one does not start with the word metric',
+                'metric broken invalid the metric type is invalid']
+METRIC_EXCEPTIONS = [('metric tooshort', IndexError),
+                     (1000, AttributeError),
+                     (iter('foo'), AttributeError)]
+
+
+def check_status(key):
+    assert ck.valid_status_type(key)
+
+
+def check_invalid_status(status):
+    test.assert_false(ck.valid_status_type(status))
+
+
+def test_valid_status_type():
+    for key in ck.STATUS_PRIORITY.iterkeys():
+        yield check_status, key
+    for status in INVALID:
+        yield check_invalid_status, status
+
+
+def check_valid_metric_type(metric_type):
+    assert ck.valid_metric_type(metric_type)
+
+
+def check_invalid_metric_type(metric_type):
+    test.assert_false(ck.valid_metric_type(metric_type))
+
+
+def test_valid_metric_type():
+    for metric_type in ck.VALID_METRICS:
+        yield check_valid_metric_type, metric_type
+    for metric_type in INVALID:
+        yield check_invalid_metric_type, metric_type
+
+
+def test_priority():
+    for status in ck.STATUS_PRIORITY.iterkeys():
+        assert ck.priority(status) == ck.STATUS_PRIORITY[status]
+    for status in INVALID:
+        test.assert_false(ck.priority(status))
+
+
+def test_compare_priority():
+    data = [s[0] for s in sorted(ck.STATUS_PRIORITY.iteritems(), key=itemgetter(1))]
+    for priority in data:
+        for lesser in data[:data.index(priority)]:
+            test.eq_(ck.compare_priority(lesser, priority), -1)
+            test.neq_(ck.compare_priority(lesser, priority), 0)
+            test.neq_(ck.compare_priority(priority, lesser), 0)
+            test.eq_(ck.compare_priority(priority, lesser), 1)
+        test.eq_(ck.compare_priority(priority, priority), 0)
+        for greater in data[(data.index(priority) + 1):]:
+            test.eq_(ck.compare_priority(priority, greater), -1)
+            test.neq_(ck.compare_priority(priority, greater), 0)
+            test.neq_(ck.compare_priority(greater, priority), 0)
+            test.eq_(ck.compare_priority(greater, priority), 1)
+
+
+def test_get_field():
+    test.eq_(ck.get_field(0, 'test 1', ck.STATUS_FIELD_COUNT), 'test')
+    test.assert_raises(IndexError, ck.get_field, 2, 'test 2', ck.STATUS_FIELD_COUNT)
+    test.assert_raises(AttributeError, ck.get_field, 1, 1000, ck.STATUS_FIELD_COUNT)
+    test.assert_raises(AttributeError, ck.get_field, 1, iter('foo'), ck.STATUS_FIELD_COUNT)
+
+
+def test_is_status_line():
+    results = [True, True, True, False, False, False]
+    for line, expected in zip(STATUS_CASES, results):
+        test.eq_(ck.is_status_line(line), expected)
+    for line, exc in STATUS_EXCEPTIONS:
+        test.assert_raises(exc, ck.is_status_line, line)
+
+
+def test_is_metric_line():
+    results = [True, True, False, False]
+    for line, expected in zip(METRIC_CASES, results):
+        test.eq_(ck.is_metric_line(line), expected)
+    for line, exc in METRIC_EXCEPTIONS:
+        test.assert_raises(exc, ck.is_metric_line, line)
+
+
+def test_get_status_type():
+    results = ['ok', 'warn', 'err', False, False, False]
+    for line, expected in zip(STATUS_CASES, results):
+        test.eq_(ck.get_status_type(line), expected)
+    for line, exc in STATUS_EXCEPTIONS:
+        test.assert_raises(exc, ck.get_status_type, line)
+
+
+def test_get_metric_type():
+    results = ['int', 'string', False, False]
+    for line, expected in zip(METRIC_CASES, results):
+        test.eq_(ck.get_metric_type(line), expected)
+    for line, exc in METRIC_EXCEPTIONS:
+        test.assert_raises(exc, ck.get_metric_type, line)
+
+
+def test_status_tuple():
+    results = [('status', 'ok', 'this is a valid ok status line'),
+               ('status', 'warn', 'this is a valid warning status line'),
+               ('status', 'err', 'this is a valid error status line'),
+               False, False, False]
+    for line, expected in zip(STATUS_CASES, results):
+        test.eq_(ck.status_tuple(line), expected)
+    for line, exc in STATUS_EXCEPTIONS:
+        test.assert_raises(exc, ck.status_tuple, line)
+
+
+def test_status_line():
+    ### At this point we've tested status_tuple() and
+    ### is_status_line(), so we can use them.
+    results = zip([ck.status_tuple(line) for line in STATUS_CASES
+                   if ck.is_status_line(line)],
+                  [line for line in STATUS_CASES
+                   if ck.is_status_line(line)])
+    for tpl, line in results:
+        test.eq_(ck.status_line(tpl), line)
+
+
+def test_metric_tuple():
+    results = [('metric', 'one', 'int', '1'),
+               ('metric', 'two', 'string', 'tea for two and two for tea'),
+               False, False]
+    for line, expected in zip(METRIC_CASES, results):
+        test.eq_(ck.metric_tuple(line), expected)
+    for line, exc in METRIC_EXCEPTIONS:
+        test.assert_raises(exc, ck.metric_tuple, line)
+
+
+def test_metric_line():
+    ### At this point we've tested metric_tuple() and
+    ### is_metric_line(), so we can use them.
+    results = zip([ck.metric_tuple(line) for line in METRIC_CASES
+                   if ck.is_metric_line(line)],
+                  [line for line in METRIC_CASES
+                   if ck.is_metric_line(line)])
+    for tpl, line in results:
+        test.eq_(ck.metric_line(tpl), line)
+
+
+def test_get_status_lines():
+    ### At this point we've tested is_status_line() so we can use it.
+    assert all([ck.is_status_line(line) for line in
+                ck.get_status_lines(STATUS_CASES)])
+
+
+def test_get_metric_lines():
+    ### At this point we've tested is_metric_line() so we can use it.
+    assert all([ck.is_metric_line(line) for line in
+                ck.get_metric_lines(METRIC_CASES)])
+
+
+def test_sort_by_priority():
+    ### At this point we've tested get_status_type() so we can use it.
+    test.eq_(ck.get_status_type(ck.sort_by_priority(STATUS_CASES)[0]), 'err')
+
+
+def test_highest_priority():
+    ### At this point we've tested get_status_type() so we can use it.
+    test.eq_(ck.get_status_type(ck.highest_priority(STATUS_CASES)), 'err')
+
+
+def test_add_status():
+    lines = [STATUS_CASES[0], METRIC_CASES[0]]
+    warn  = STATUS_CASES[1]
+    err = STATUS_CASES[2]
+    test.eq_(ck.add_status(warn, lines)[0], warn)
+    test.eq_(ck.add_status(err, lines)[0], err)
