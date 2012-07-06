6f75a84643cc8f12f0ce0e7e6a07834cffd135e2
Charlie Schluting <charlie@schluting.com>
2012-07-06 13:32:07 -0700

    .

--- monitorlib/examples/collectd_check.py
+++ monitorlib/examples/collectd_check.py
@@ -14,17 +14,17 @@ import monitorlib.collectd as collectd
 if __name__ == '__main__':
 
     # fake a check, that outputs True/False:
-    check_value = None
+    check_value = False
 
 
     if check_value:
-        print collectd.ok("everything is fine")
+        print collectd.ok("everything is fine", pageme=True)
 
     elif check_value is False:
-        print collectd.warning("something looks strange")
+        print collectd.warning("something looks strange", False, email=['charlie@krux.com'])
 
     elif check_value is None:
-        print collectd.failure("something is definitely broken", pageme=True)
+        print collectd.failure("something is definitely broken", page=True, email=['charlie@krux.com'])
 
     # output a metric:
     print collectd.metric("testing/service_bar", 5)
