5a38e27d50dbe86a870dc9557b27821fd2da0291
Charlie Schluting <charlie@schluting.com>
2012-07-06 13:31:38 -0700

    changes to support email=True flow-through.

--- monitorlib/collectd.py
+++ monitorlib/collectd.py
@@ -10,10 +10,17 @@
 
  Then, you can use collectd.failure("message content"),
  and the lib will output a notification in the correct format (timestamp, hostname, etc).
- If the optional 2nd argument ("pageme") is True, pagerduty will be triggered.
 
  likewise, you can use it to output values:
  collectd.metric("path/to/metric", value)
+
+ ==
+ The following options require using bin/collectd_notify as the notification plugin:
+
+ If the optional 2nd argument ("page") is True, pagerduty will be triggered.
+
+ A 3rd, optional argument, can be an email address to notify, or list of emails.
+
 """
 
 import time
@@ -27,22 +34,26 @@ INTERVAL = os.environ.get('COLLECTD_INTERVAL', "60")
 #CALLER = os.path.basename(inspect.stack()[-1][1])
 CALLER = os.path.basename(sys.argv[0])
 
-def notify_output(severity, message, pageme=False):
+def notify_output(severity, message, page, email):
     ''' formats and returns a collectd notification (str) '''
     timestamp = int(time.mktime(time.gmtime()))
-    message = "%s: %s" % (CALLER, message)
-    if pageme: message = 'PD' + message
+    message = "%s %s: %s" % (FQDN.split('.')[0], CALLER, message)
+
+    if page:
+        message = 'PD' + message
+    if email:
+        message = str(email) + 'ENDEMAIL' + message
 
     return "PUTNOTIF severity=%s time=%i host=%s message=%s" % (severity, timestamp, FQDN, message)
 
-def failure(string, pageme=False):
-    return notify_output('failure', string, pageme)
+def failure(string, page=False, email=False):
+    return notify_output('failure', string, page, email)
 
-def warning(string, pageme=False):
-    return notify_output('warning', string, pageme)
+def warning(string, page=False, email=False):
+    return notify_output('warning', string, page, email)
 
-def ok(string, pageme=False):
-    return notify_output('okay', string, pageme)
+def ok(string, page=False, email=False):
+    return notify_output('okay', string, page, email)
 
 def metric(path, value):
     ''' formats and returns a collectd metric value (str) '''
