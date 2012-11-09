98cd38d52a2f50f04f79563b0f53503ef0f4c832
Charlie Schluting <charlie@schluting.com>
2012-11-08 16:08:17 -0800

    bumping vers

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@
 from setuptools import setup, find_packages
 
 setup(name='monitorlib',
-      version="0.1.6",
+      version="0.1.7",
       description='Library for creating monitoring scripts/plugins',
-      author='Paul Lathrop',
-      author_email='paul@krux.com',
+      author='Paul Lathrop, Charlie Schluting',
+      author_email='paul@krux.com, charlie@krux.com',
       url='https://github.com/krux/monitorlib',
       packages=find_packages(),
       tests_require=['nose', 'coverage'],
