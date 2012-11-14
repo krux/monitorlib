b0fc24514f1727ed244f745dfd3aeafb2ad63dcb
Charlie Schluting <charlie@schluting.com>
2012-11-14 10:32:06 -0800

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 setup(name='monitorlib',
-      version="0.1.7",
+      version="0.2.0",
       description='Library for creating monitoring scripts/plugins',
       author='Paul Lathrop, Charlie Schluting',
       author_email='paul@krux.com, charlie@krux.com',
