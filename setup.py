46953052e62742a3797da7b07bc2af68c34d81f4
Paul Lathrop <paul@tertiusfamily.net>
2014-06-02 10:07:45 -0700

    Fix dependencies.

--- setup.py
+++ setup.py
@@ -1,32 +1,24 @@
 ### -*- coding: utf-8 -*-
 ###
-### © 2012 Krux Digital, Inc.
-### Author: Paul Lathrop <paul@krux.com>
+### © 2012, 2014 Krux Digital, Inc.
+### Author: Charlie Schluting <charlie@krux.com>
 ###
-import os
-from pip.req    import parse_requirements
+
 from setuptools import setup, find_packages
 
-# We want to install all the dependencies of the library as well, but we
-# don't want to duplicate the dependencies both here and in
-# requirements.pip. Instead we parse requirements.pip to pull in our
-# dependencies.
-BASE_DIR     = os.path.dirname(os.path.abspath(__file__))
-REQUIREMENTS = os.path.join(BASE_DIR, 'requirements.pip')
 
-# A requirement file can contain comments (#) and can include some other
-# files (--requirement or -r), so we need to use pip's parser to get the
-# final list of dependencies.
-DEPENDENCIES = [unicode(package.req)
-                for package in parse_requirements(REQUIREMENTS)]
-
-setup(name='monitorlib',
-      version="0.2.14",
-      description='Library for creating monitoring scripts/plugins',
-      author='Paul Lathrop, Charlie Schluting',
-      author_email='paul@krux.com, charlie@krux.com',
-      url='https://github.com/krux/monitorlib',
-      install_requires = DEPENDENCIES,
-      packages=find_packages(),
-      tests_require=['nose', 'coverage'],
-      )
+setup(
+    name='monitorlib',
+    version="0.2.15",
+    description='Library for creating monitoring scripts/plugins',
+    author='Charlie Schluting',
+    author_email='charlie@krux.com',
+    url='https://github.com/krux/monitorlib',
+    install_requires=[
+        'bernhard',
+        'redis',
+        'simplejson',
+    ],
+    packages=find_packages(),
+    tests_require=['nose', 'coverage'],
+)
